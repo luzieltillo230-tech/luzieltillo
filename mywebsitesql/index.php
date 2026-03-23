c176f28d69b04dd8be590fb5e0195da141039ba7
luzieltillo230-tech <luzieltillo230@gmail.com>
2026-03-23 18:49:59 +0800

    Add files via upload

---
+++ mywebsitesql/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title> Luziel S. Tillo</title>
+</head>
+<body>
+    <h1> Username:</h1>
+    <h2> Password:</h2>
+</body>
+<p> Log-in
+</html>
